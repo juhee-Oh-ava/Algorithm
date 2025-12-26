8f70699392c434784dab1c30cd0a7d7a870bc76f
juhee-Oh-ava <zchrx7625@naver.com>
2025-12-26 21:30:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[K];
+        long max = 0;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long left = 1;
+        long right = max;
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+
+            for (long len : arr) {
+                count += len / mid;
+            }
+
+            if (count >= N) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
