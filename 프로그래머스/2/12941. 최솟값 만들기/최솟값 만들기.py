a7cee10b16dbc10f77ae10d669f6322a77e41f84
juhee-Oh-ava <zchrx7625@naver.com>
2026-07-23 22:12:19 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.53 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,11 +1,20 @@
+import heapq
+
 def solution(A,B):
     answer = 0
-
-    A.sort()
-    B.sort(reverse=True)
     
-    for i in range(len(A)):
-        answer += A[i] * B[i]
-            
+    heapq.heapify(A)  # A 자체가 최소 힙
+    heapB = []
+    
+    for i in B:
+        heapq.heappush(heapB, -i) # heap[0]에는 항상 가장 작은 값이 있다. 
+        
+    
+    while heapB and A:
+        min_a = heapq.heappop(A) 
+        max_b = -heapq.heappop(heapB)
+
+        answer += min_a * max_b
+
 
     return answer
